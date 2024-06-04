3b7638dcc1cc6c6ae411fcdccc4d1ea1db9f725a
sanket560 <sanketmmane@gmail.com>
2024-06-05 02:14:49 +0530

    fixed force dynamic issue

--- src/app/api/report-by-id/route.js
+++ src/app/api/report-by-id/route.js
@@ -2,6 +2,8 @@ import connectDB from "@/database/dbConfig";
 import Report from "@/models/Report.Model";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req) {
   try {
     await connectDB();
